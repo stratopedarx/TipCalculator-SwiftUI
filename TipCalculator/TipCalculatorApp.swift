5ba5c13043ec553d2deae598d535be2d64df7745
Sergey Lobanov <sergey.lobanov@orioninc.com>
2021-10-20 15:01:07 +0300

    Initial Commit

---
+++ TipCalculator/TipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorApp.swift
+//  TipCalculator
+//
+//  Created by Sergey Lobanov on 20.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
